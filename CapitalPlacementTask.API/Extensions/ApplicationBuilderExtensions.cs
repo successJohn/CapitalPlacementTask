78738603783894d5df4aba1a28b330568acbc3c1
successJohn <successojohn@gmail.com>
2023-10-08 21:10:28 +0100

    Global Exception Handler

---
+++ CapitalPlacementTask.API/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using CapitalPlacementTask.Application.Utilities;
+
+namespace CapitalPlacementTask.API.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder AddGlobalErrorHandler(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<GlobalExceptionHandler>();
+        }
+    }
+}
---
+++ CapitalPlacementTask.Application/Utilities/GlobalExceptionHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+namespace CapitalPlacementTask.Application.Utilities
+{
+    public class GlobalExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(RequestDelegate next, ILogger<GlobalExceptionHandler> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            var responseMessage = exception.Message;
+            
+            _logger.LogError($"Something went wrong: {exception.Message} {exception.StackTrace}");
+ 
+            return context.Response.WriteAsJsonAsync(new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error"
+            });
+
+        }
+    }
+}
